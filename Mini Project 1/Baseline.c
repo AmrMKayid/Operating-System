e5fea113e08380b9cd4f7f95675fd14ba0353b1f
Amr M. Kayid <amrkayid2027@gmail.com>
2018-02-14 01:16:56 +0200

    Documente the program

--- Mini Project 1/Baseline.c
+++ Mini Project 1/Baseline.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
 #include <sys/stat.h>
@@ -18,6 +18,7 @@ int main(int argc, char * argv[])   {
 
     char * directoryName = argv[1];  /* Directory name to be created */
 
+    /* Create Directory if it does not exist */
     if (stat(directoryName, &st) == -1) {
         mkdir(directoryName, 0700);
     }
@@ -26,6 +27,8 @@ int main(int argc, char * argv[])   {
         if (n < 0)
             perror("scandir");
         else {
+            /* scan the files that start with the same name
+             to get the index to create new duplicate Directory */
             for (i = 0; i < n; i++) {
                 char *s = namelist[i]->d_name;
                 if (strncmp(s, directoryName, strlen(directoryName)) == 0)
@@ -35,6 +38,7 @@ int main(int argc, char * argv[])   {
             }
             free(namelist);
 
+            /* create the new duplicate name directory with index */
             int length = snprintf( NULL, 0, "%d", indx );
             char *s2 = malloc( length + 1 );
             snprintf( s2, length + 1, "%d", indx );
