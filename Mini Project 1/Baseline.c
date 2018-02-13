f4c2e6a3e16601ed7a6ebce1c974fa4ad2e065a7
Amr M. Kayid <amrkayid2027@gmail.com>
2018-02-14 01:12:18 +0200

    Finish Basline C Program

--- Mini Project 1/Baseline.c
+++ Mini Project 1/Baseline.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+
+
 /*
 Other include statements may be needed !
 */
 
-void main(int argc, char * argv[])   { 
+struct stat st = {0};
+struct dirent **namelist;
+int i, n, indx = 0;
+
+int main(int argc, char * argv[])   { 
 
 
-char * directoryName = argv[1];  /* Directory name to be created */
+    char * directoryName = argv[1];  /* Directory name to be created */
 
-	/*
-		Rest of implementation goes here
-	*/
+    if (stat(directoryName, &st) == -1) {
+        mkdir(directoryName, 0700);
+    }
+    else {
+        n = scandir(".", &namelist, 0, alphasort);
+        if (n < 0)
+            perror("scandir");
+        else {
+            for (i = 0; i < n; i++) {
+                char *s = namelist[i]->d_name;
+                if (strncmp(s, directoryName, strlen(directoryName)) == 0)
+                    indx++;
+                free(namelist[i]);
+                }
+            }
+            free(namelist);
+
+            int length = snprintf( NULL, 0, "%d", indx );
+            char *s2 = malloc( length + 1 );
+            snprintf( s2, length + 1, "%d", indx );
+            mkdir(strcat(directoryName,s2), 0700);
+            free(s2);
+        }
 }
