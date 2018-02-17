36c51a3e3e47279323c8858e8bc977f05510c5e5
Amr M. Kayid <amrkayid2027@gmail.com>
2018-02-17 21:07:02 +0200

    Changed the algorithm

--- Mini Project 1/Baseline.c
+++ Mini Project 1/Baseline.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
 #include <sys/stat.h>
@@ -10,48 +10,30 @@ Other include statements may be needed !
 */
 
 struct stat st = {0};
-struct dirent **namelist;
-int i, n, indx = 0;
+struct dirent **folders;
+int isFound, indx = 0;
 char *newDirectoryName;
 
 void concIntToString(char *s, int indx);
 
 int main(int argc, char * argv[])   { 
 
-
     char * directoryName = argv[1];  /* Directory name to be created */
     newDirectoryName = malloc(strlen(directoryName) + 1);
     strcpy(newDirectoryName, directoryName);
 
-    if (stat(directoryName, &st) == -1) {
-        mkdir(directoryName, 0700);
-    }
-    else {
-        n = scandir(".", &namelist, 0, alphasort);
-        if (n < 0)
-            perror("scandir");
-        else {
-
-            for (i = 0; i < n; i++) {
-                char *s = namelist[i]->d_name;
-
-                if (strncmp(s, directoryName, strlen(s)) == 0) {
-                    indx++;
-                    strcpy(directoryName, newDirectoryName);
-                    concIntToString(directoryName, indx);
-                    // printf("%s\n", directoryName);
-                }
-                free(namelist[i]);
-                }
-            }
-            free(namelist);
-
-            concIntToString(newDirectoryName, indx);
-            // printf("%s\n", newDirectoryName);
-
-            mkdir(newDirectoryName, 0700);
-
+    while (1) {
+        isFound = scandir(directoryName, &folders, 0, alphasort);
+        if (isFound == -1) {
+            mkdir(directoryName, 0700);
+            return 0;
         }
+        else {
+            indx++;
+            strcpy(directoryName, newDirectoryName);
+            concIntToString(directoryName, indx);
+        }
+    }
 }
 
 void concIntToString(char *s, int indx) {
@@ -59,5 +41,4 @@ void concIntToString(char *s, int indx) {
     snprintf( s2, indx + 1, "%d", indx );
     strcat(s,s2);
     free(s2);
-    // return s;
 }
