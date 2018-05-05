57fd7cc4ada26717f7a5a8a5ee89fe21604b057b
Amr M. Kayid <amrkayid2027@gmail.com>
2018-05-05 22:13:50 +0200

    Add read file function

--- Mini Project 4/PageReplacement/main.c
+++ Mini Project 4/PageReplacement/main.c
@@ -1,6 +1,69 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <memory.h>
+
+int getFileLines(char *filename);
+void readCSV(char *filename);
 
 int main() {
-    printf("Hello, World!\n");
+
+    char *FILENAME = "/Users/amrmkayid/Desktop/PageReplacement/pages.txt";
+    readCSV(FILENAME);
+
     return 0;
+}
+
+void readCSV(char *filename) {
+
+    FILE *fPointer;
+    fPointer = fopen(filename, "r");
+
+    int lines = getFileLines(filename) + 2;
+
+    char singleLine[1024];
+
+
+    int k = 0;
+
+    char sentences[lines][1024];
+
+    while (!feof(fPointer)) {
+        fgets(singleLine, 1024, fPointer);
+        strcpy(sentences[k], singleLine);
+        k++;
+    }
+
+    for (int l = 0; l < k; ++l) {
+        char *a[3];
+        for (int i = 0; i < 3; ++i) {
+            a[i] = strtok(sentences[l], ",");
+
+            while (a[i] != NULL)
+                a[++i] = strtok(NULL, ",");
+
+        }
+        printf(">>> %s | %s | %s \n", a[0], a[1], a[2]);
+    }
+
+    fclose(fPointer);
+}
+
+
+int getFileLines(char *filename) {
+    // count the number of lines in the file called filename
+    FILE *fp = fopen(filename, "r");
+    int ch = 0;
+    int lines = 0;
+
+    if (fp == NULL)
+        return 0;
+
+    lines++;
+    while ((ch = fgetc(fp)) != EOF) {
+        if (ch == '\n')
+            lines++;
+    }
+    fclose(fp);
+    return lines;
 }
