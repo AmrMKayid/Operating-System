1c3ae669fbf0e613891cb2dc0e9d652f5515e9f7
Amr M. Kayid <amrkayid2027@gmail.com>
2018-05-06 18:04:44 +0200

    Remove linkedlist

--- Mini Project 4/PageReplacement/main.c
+++ Mini Project 4/PageReplacement/main.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
 #include <memory.h>
 #include <stdbool.h>
 
 typedef struct page Page;
-typedef struct node Node;
 
 unsigned numOfFrames;
 unsigned numOfPages;
 
 int getFileLines(char *filename);
 
-void readCSV(Node *start, char *filename);
-
-void push(Node **head_ref, void *new_data, size_t data_size);
-
-void printList(Node *node, void (*fptr)(void *));
+void readCSV(char *filename);
 
 Page *newPage(int accessTime, int pageID, char *bitType);
 
@@ -57,44 +51,9 @@ void printPage(Page *p) {
 /*************************************/
 
 
-/******* LinkedList of Pages ********/
-
-typedef struct node {
-    // Any data type can be stored in this node
-    void *data;
-
-    struct node *next;
-} Node;
-
-void push(Node **head_ref, void *new_data, size_t data_size) {
-    // Allocate memory for node
-    Node *new_node = (Node *) malloc(sizeof(Node));
-
-    new_node->data = malloc(data_size);
-    new_node->next = (*head_ref);
-
-    // Copy contents of new_data to newly allocated memory.
-    // Assumption: char takes 1 byte.
-    int i;
-    for (i = 0; i < data_size; i++)
-        *(char *) (new_node->data + i) = *(char *) (new_data + i);
-
-    // Change head pointer as new node is added at the beginning
-    (*head_ref) = new_node;
-}
-
-void printList(Node *node, void (*fptr)(void *)) {
-    while (node != NULL) {
-        (*fptr)(node->data);
-        node = node->next;
-    }
-}
-
-/*************************************/
-
 bool isFound(Page listOfPages[5], Page *p, int size) {
-    for(int i = 0; i < size; ++i) {
-        if(listOfPages[i].pageID == p->pageID)
+    for (int i = 0; i < size; ++i) {
+        if (listOfPages[i].pageID == p->pageID)
             return true;
     }
     return false;
@@ -107,7 +66,7 @@ void pop(Page listOfPages[5], int size, int index) {
     index--;
 }
 
-void readCSV(Node *start, char *filename) {
+void readCSV(char *filename) {
 
     FILE *fPointer;
     fPointer = fopen(filename, "r");
@@ -146,17 +105,13 @@ void readCSV(Node *start, char *filename) {
          * FIFO page replacement algorithm
          */
 
-        if(index < numOfFrames) {
+        if (index < numOfFrames) {
             Page *p = newPage(accessTime, pageID, bitType);
-            if(!isFound(&listOfPages, p, numOfFrames))
+            if (!isFound(&listOfPages, p, numOfFrames))
                 listOfPages[index++] = p;
         } else {
             pop(&listOfPages, numOfFrames, index);
         }
-
-//        printPage(listOfPages[--index]);
-//        push(&start, newPage(accessTime, pageID, bitType), page_size);
-
     }
 
     fclose(fPointer);
@@ -188,9 +143,7 @@ int main() {
 
     numOfFrames = 5;
 
-    Node *start = NULL;
-
-    readCSV(start, FILENAME);
+    readCSV(FILENAME);
 
     return 0;
 }
