b5cf8bb6b728fca7600eaac6b023afbe1467f4a0
Amr M. Kayid <amrkayid2027@gmail.com>
2018-05-06 20:05:28 +0200

    Finish Second-Chance Algorithm

--- Mini Project 4/PageReplacement/main.c
+++ Mini Project 4/PageReplacement/main.c
@@ -31,7 +31,6 @@ void printPage(Page *p) {
     printf("%2u (pageID: %2u) %2u %2u\n", p->accessTime, p->pageID, p->referencedBit, p->modifiedBit);
 }
 
-
 int getFileLines(char *filename) {
     // count the number of lines in the file called filename
     FILE *fp = fopen(filename, "r");
@@ -84,7 +83,6 @@ void fifoAlgorithm(char *filename) {
         char *bitType = a[2];
 
         Page *p = newPage(accessTime, pageID, bitType);
-//        printPage(p);
 
         /*
          * FIFO Algorithm
@@ -102,6 +100,9 @@ void fifoAlgorithm(char *filename) {
                 printPage(p);
             } else {
                 printf("This Page is found in the list: ");
+                printPage(p);
+                listOfPages[0] = p;
+                printf("Updating the founded Page: ");
                 printPage(p);
             }
 
@@ -126,6 +127,9 @@ void fifoAlgorithm(char *filename) {
             } else {
                 printf("This Page is found in the list: ");
                 printPage(p);
+                listOfPages[0] = p;
+                printf("Updating the founded Page: ");
+                printPage(p);
             }
         }
 
@@ -135,11 +139,193 @@ void fifoAlgorithm(char *filename) {
 }
 
 
+void secondChanceAlgorithm(char *filename) {
+
+    FILE *fPointer;
+    fPointer = fopen(filename, "r");
+
+    int lines = getFileLines(filename);
+
+    char singleLine[1024];
+
+    int k = 0;
+
+    char sentences[lines][1024];
+
+    while (!feof(fPointer)) {
+        fgets(singleLine, 1024, fPointer);
+        strcpy(sentences[k], singleLine);
+        k++;
+    }
+
+    Page *listOfPages[5];
+    int counter = 0;
+
+    for (int l = 0; l < k - 1; ++l) {
+        char *a[3];
+        for (int i = 0; i < 3; ++i) {
+            a[i] = strtok(sentences[l], ",");
+
+            while (a[i] != NULL)
+                a[++i] = strtok(NULL, ",");
+
+        }
+        int accessTime = atoi(a[0]), pageID = atoi(a[1]);
+        char *bitType = a[2];
+
+        Page *p = newPage(accessTime, pageID, bitType);
+
+        /*
+         * second-chance page replacement algorithms.
+         */
+        bool found = false;
+        if (counter < 5) {
+            for (int i = 0; i < counter; ++i) {
+                if (listOfPages[i]->pageID == p->pageID)
+                    found = true;
+            }
+
+            if (!found) {
+                listOfPages[counter++] = p;
+                printf("Added new Page to the list: ");
+                printPage(p);
+
+            } else {
+                printf("This Page is found in the list: ");
+                printPage(p);
+                listOfPages[0] = p;
+                printf("Updating the founded Page: ");
+                printPage(p);
+            }
+
+        } else {
+
+            for (int i = 0; i < counter; ++i) {
+                if (listOfPages[i]->pageID == p->pageID)
+                    found = true;
+            }
+
+            if (!found) {
+
+                while (true) {
+                    Page *firstPage = listOfPages[0];
+
+                    /*
+                     * Check for first page
+                     * ReferenceBit = 0 && ModifiedBit = 0
+                     */
+                    if (firstPage->referencedBit == 0 && firstPage->modifiedBit == 0) {
+                        printf("Removing this Page from the list: ");
+                        printPage(listOfPages[0]);
+                        for (int i = 0; i < counter; ++i) {
+                            listOfPages[i] = listOfPages[i + 1];
+                        }
+
+                        listOfPages[counter - 1] = p;
+                        printf("Added new Page to the list: ");
+                        printPage(p);
+
+                        break;
+                    }
+
+                        /*
+                         * Check for first page
+                         * ReferenceBit = 1 && ModifiedBit = X
+                         */
+                    else if (firstPage->referencedBit == 1) {
+                        Page *newPage = firstPage;
+                        newPage->referencedBit = 0;
+
+
+                        for (int i = 0; i < counter; ++i) {
+                            listOfPages[i] = listOfPages[i + 1];
+                        }
+
+                        listOfPages[counter - 1] = newPage;
+                        printf("Removing the page from the front and adding it to the back with (R-Bit = 0) : ");
+                        printPage(newPage);
+
+                    }
+
+                        /*
+                         * Check for first page
+                         * ReferenceBit = 0 && ModifiedBit = 1
+                         * Search for R-bit = 0 && M-bit = 0 then remove it
+                         */
+
+                    else if (firstPage->referencedBit == 0 && firstPage->modifiedBit == 1) {
+
+                        int index = 0;
+
+                        bool f = false;
+
+                        for (int i = 0; i < counter; ++i) {
+                            if (listOfPages[i]->referencedBit == 0 && listOfPages[i]->modifiedBit == 0) {
+                                index = i;
+                                f = true;
+                                break;
+                            }
+                        }
+
+                        if(f) {
+                            for (; index < counter; ++index)
+                                listOfPages[index] = listOfPages[index + 1];
+
+
+                            listOfPages[counter - 1] = p;
+                            printf("Removing the page from the front and adding it to the back with (R-Bit = 0) : ");
+                            printPage(p);
+                        } else {
+                            printf("Removing the first Page from the list: ");
+                            printPage(listOfPages[0]);
+                            for (int i = 0; i < counter; ++i) {
+                                listOfPages[i] = listOfPages[i + 1];
+                            }
+
+
+                            listOfPages[counter - 1] = p;
+                            printf("Added new Page to the list: ");
+                            printPage(p);
+                        }
+
+                        break;
+                    }
+
+                }
+
+            } else {
+
+
+                printf("This Page is found in the list: ");
+                printPage(p);
+                listOfPages[0] = p;
+                printf("Updating the founded Page: ");
+                printPage(p);
+            }
+        }
+
+        if(p->accessTime + 1 % 20 == 0) {
+            for (int i = 0; i < counter; ++i) {
+                listOfPages[i]->referencedBit = 0;
+            }
+        }
+
+    }
+
+    fclose(fPointer);
+}
+
 int main() {
 
     char *FILENAME = "/Volumes/Life/GUC/Projects/OS/Mini Project 4/PageReplacement/pages.txt";
 
+
+    printf("Fifo Algorithm\n\n");
     fifoAlgorithm(FILENAME);
+
+    printf("\n\nsecond-chance page replacement algorithms.\n\n");
+
+    secondChanceAlgorithm(FILENAME);
 
     return 0;
 }
